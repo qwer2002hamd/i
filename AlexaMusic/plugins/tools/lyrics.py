b8d37f97d15999a2350ddeea04c487fa44b4e300
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-01 14:35:41 +0300

    lyrics.py

--- AlexaMusic/plugins/tools/lyrics.py
+++ AlexaMusic/plugins/tools/lyrics.py
@@ -35,7 +35,11 @@ y = lg.Genius(
 y.verbose = False
 
 
-@app.on_message(filters.command(LYRICS_COMMAND) & ~filters.edited & ~BANNED_USERS)
+@app.on_message(
+ filters.command(LYRICS_COMMAND,"")
+    & filters.private
+    & ~filters.edited
+    & ~BANNED_USERS)
 @language
 async def lrsearch(client, message: Message, _):
     if len(message.command) < 2:

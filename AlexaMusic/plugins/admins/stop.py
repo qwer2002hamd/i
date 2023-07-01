8333161a7b7aef32061071e303a30b0e7091a492
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-01 09:32:57 +0300

    stop.py

--- AlexaMusic/plugins/admins/stop.py
+++ AlexaMusic/plugins/admins/stop.py
@@ -39,17 +39,22 @@ async def stop_music(cli, message: Message, _, chat_id):
         _["admin_9"].format(message.from_user.first_name),
         reply_markup=close_keyboard,
     )
+
+
+
+
+
 @app.on_message(
  filters.command(STOP_COMMAND,"")
     & ~filters.edited
     & filters.channel
     & ~BANNED_USERS
 )
-@AdminRightsCheckk
+@AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Anon.stop_stream(chat_id)
+    await Alexa.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.first_name),

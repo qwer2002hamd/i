6cc84fa64c2f9c2d1a0f243ab34935b3a4527dfd
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-01 13:15:17 +0300

    private.py

--- AlexaMusic/plugins/sudo/private.py
+++ AlexaMusic/plugins/sudo/private.py
@@ -29,7 +29,10 @@ UNAUTHORIZE_COMMAND = get_command("UNAUTHORIZE_COMMAND")
 AUTHORIZED_COMMAND = get_command("AUTHORIZED_COMMAND")
 
 
-@app.on_message(filters.command(AUTHORIZE_COMMAND) & SUDOERS)
+@app.on_message(
+ filters.command(AUTHORIZE_COMMAND,"")
+    & ~filters.edited
+    & ~BANNED_USERS)
 @language
 async def authorize(client, message: Message, _):
     if config.PRIVATE_BOT_MODE != str(True):
@@ -47,7 +50,10 @@ async def authorize(client, message: Message, _):
         await message.reply_text(_["pbot_5"])
 
 
-@app.on_message(filters.command(UNAUTHORIZE_COMMAND) & SUDOERS)
+@app.on_message(
+ filters.command(UNAUTHORIZE_COMMAND,"")
+    & ~filters.edited
+    & ~BANNED_USERS)
 @language
 async def unauthorize(client, message: Message, _):
     if config.PRIVATE_BOT_MODE != str(True):
@@ -65,7 +71,10 @@ async def unauthorize(client, message: Message, _):
         return await message.reply_text(_["pbot_4"])
 
 
-@app.on_message(filters.command(AUTHORIZED_COMMAND) & SUDOERS)
+@app.on_message(
+ filters.command(AUTHORIZED_COMMAND,"")
+    & ~filters.edited
+    & ~BANNED_USERS)
 @language
 async def authorized(client, message: Message, _):
     if config.PRIVATE_BOT_MODE != str(True):

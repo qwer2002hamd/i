b6866b311859a47a46a4de4569e0dcbe9dd19f27
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-01 13:45:29 +0300

    autoend.py

--- AlexaMusic/plugins/sudo/autoend.py
+++ AlexaMusic/plugins/sudo/autoend.py
@@ -24,8 +24,8 @@ AUTOEND_COMMAND = get_command("AUTOEND_COMMAND")
 
 @app.on_message(
  filters.command(AUTOEND_COMMAND,"")
-    & ~filters.edited
-    & ~BANNED_USERS)
+    & ~BANNED_USERS
+)
 async def auto_end_stream(client, message):
     usage = "**ᴜsᴀɢᴇ:**\n\n/autoend [enable|disable]"
     if len(message.command) != 2:
